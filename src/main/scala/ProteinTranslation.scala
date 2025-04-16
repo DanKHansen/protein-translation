ef21d94366519351c9db28ec6d8914b6fa87c479
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 09:00:38 +0200

    refactor

--- src/main/scala/ProteinTranslation.scala
+++ src/main/scala/ProteinTranslation.scala
@@ -1,23 +1,14 @@
-import scala.annotation.tailrec
-
 object ProteinTranslation:
-   private val pMap: Map[Seq[String], String] = Map(
-     Seq("AUG") -> "Methionine",
-     Seq("UUU", "UUC") -> "Phenylalanine",
-     Seq("UUA", "UUG") -> "Leucine",
-     Seq("UCU", "UCC", "UCA", "UCG") -> "Serine",
-     Seq("UAU", "UAC") -> "Tyrosine",
-     Seq("UGU", "UGC") -> "Cysteine",
-     Seq("UGG") -> "Tryptophan",
-     Seq("UAA", "UAG", "UGA") -> "STOP"
-   )
-   def proteins(s: String): Seq[String] =
-      @tailrec
-      def loop(cs: Seq[String], proteins: Seq[String]): Seq[String] =
-         cs match
-            case Nil                                        => proteins.reverse
-            case ::(head, _) if pMap.last._1.contains(head) => proteins.reverse
-            case ::(head, tail)                             =>
-               loop(tail, proteins.prepended(pMap.find(_._1.contains(head)).map(_._2).getOrElse("")))
+   private def codonMatch(codon: String): String =
+      codon match
+         case "AUG"                         => "Methionine"
+         case "UUU" | "UUC"                 => "Phenylalanine"
+         case "UUA" | "UUG"                 => "Leucine"
+         case "UCU" | "UCC" | "UCA" | "UCG" => "Serine"
+         case "UAU" | "UAC"                 => "Tyrosine"
+         case "UGU" | "UGC"                 => "Cysteine"
+         case "UGG"                         => "Tryptophan"
+         case "UAA" | "UAG" | "UGA"         => "STOP"
 
-      loop(s.grouped(3).toSeq, Nil)
+   def proteins(s: String): Seq[String] =
+      s.grouped(3).map(codonMatch).takeWhile(_ != "STOP").toSeq
